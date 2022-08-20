83424b93a2bc1dda99f4bcb003e5c9dba4da1d4e
RobertooMota <robertomota.contato@gmail.com>
2022-08-20 19:21:46 -0300

    Desafio reconhecimento concluido

--- 2022-08-12 - Exercicio 4/ex2.c
+++ 2022-08-12 - Exercicio 4/ex2.c
@@ -3,9 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
-int checkNota(float);
-int checkNome(char *nome);
 int check(char *dado, int tipo); // tipo 1 nome tipo 2 nota
+int checkMaior(char *nome, char *nota);
 
 int main(int argc, char *argv[])
 {
@@ -13,14 +12,22 @@ int main(int argc, char *argv[])
     float maiorNota = 0;
     int index, statusNome, statusNota;
 
-    printf("Tamanho do argC %d\n\n", argc);
-    if (argc >= 2)
+    // printf("Tamanho do argC %d\n\n", argc);
+    if (argc > 2)
     {
         for (index = 1; index < argc; index++)
         {
             if (index % 2 == 0)
             {
                 statusNota = check(argv[index], 2);
+
+                //...........
+                if (statusNome && statusNota)
+                {
+                    checkMaior(argv[index - 1], argv[index]);
+                }
+
+                //...........
             }
             else
             {
@@ -28,16 +35,13 @@ int main(int argc, char *argv[])
             }
         }
     }
-
     else
     {
-        printf("\nArg C eh menor que 3\n");
+        printf("\nArgc tem menos parametros do que deveria!!!\n");
     }
 
-    if (statusNome && statusNota)
-    {
-    }
-    printf("\n\nstatus nota: %d\nstatus nome: %d\n", statusNota, statusNome);   // <<<<<---- diz se a nota é um numero
+    // Status 1 = tudo OK Status 2 = Falha na verificação
+    // printf("\nstatus nota: %d\nstatus nome: %d\n", statusNota, statusNome); //<<<<---- Descomentar para debugar
     return 0;
 }
 
@@ -54,7 +58,7 @@ int check(char *dado, int tipo)
                 return 0;
             }
         }
-        printf("eh um numero (tipo 2)");
+        printf("eh um numero (tipo 2)\n");
         return 1;
     }
     else if (tipo == 1)
@@ -62,7 +66,7 @@ int check(char *dado, int tipo)
         int index;
         for (index = 0; index < strlen(dado); index++)
         {
-            if(isdigit(dado[index]))
+            if (isdigit(dado[index]))
             {
                 printf("Tem numero\n");
                 return 0;
@@ -73,10 +77,7 @@ int check(char *dado, int tipo)
     }
 }
 
-// strcpy(melhorAluno, argv[1]);
-// int index;
-// for (index = 0; index <= argc; index++)
-// {
-
-// printf("%d",index);
-// }
+int checkMaior(char *nome, char *nota)
+{
+    return 1;
+}
